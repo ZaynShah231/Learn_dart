481ab5007eef393f3fed5fb47f242bf4686278ca
SyedZainulabideen <shahidzain014@gmail.com>
2025-11-07 17:15:26 +0500

    Dart Calculator Code

---
+++ Calculator.dart
@@ -0,0 +1,41 @@
+import 'dart:io';
+
+void main() {
+  print("Calculator");
+  print("1. Addition");
+  print("2. Subtraction");
+  print("3. Multiplication");
+  print("4. Division");
+
+  stdout.write("Enter your choice (1-4): ");
+  String? choice = stdin.readLineSync();
+
+  stdout.write("Enter first number: ");
+  double num1 = double.parse(stdin.readLineSync()!);
+
+  stdout.write("Enter second number: ");
+  double num2 = double.parse(stdin.readLineSync()!);
+
+  switch (choice) {
+    case '1':
+      print("Result: ${num1 + num2}");
+      break;
+    case '2':
+      print("Result: ${num1 - num2}");
+      break;
+    case '3':
+      print("Result: ${num1 * num2}");
+      break;
+    case '4':
+      if (num2 != 0) {
+        print("Result: ${num1 / num2}");
+      } else {
+        print("Error: Division by zero!");
+      }
+      break;
+    default:
+      print("Invalid choice!");
+  }
+}
+
+
